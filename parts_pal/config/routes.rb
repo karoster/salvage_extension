99b11caffa436c156ac0289d68b0245563b13b7e
karoster <kosterb1@jhu.edu>
2020-02-05 17:23:52 -0500

    prep for heroku deploy

--- parts_pal/config/routes.rb
+++ parts_pal/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :parts, only: [:index, :create]
     end
   end
-  root 'homepage#index'
+  # root 'homepage#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
